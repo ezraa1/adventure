3aee289aa27db256f8045650372ade2691cc9008
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-06 21:01:20 +0300

    add routes to open server

--- back-end/db/migrate/20220906101127_create_comments.rb
+++ back-end/db/migrate/20220906101127_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.text :comment
+      t.string :text
       t.integer :post_id
     end
   end
--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -15,10 +15,10 @@ post4 = Post.create(date: "5-9-2022", title:" Night Photography: Tips and Tricks
 post5 = Post.create(date: "30-9-2022", title:" Travel Because it is fun!", content:"Adventure with us on our ten day road trip through Norway! Set aside twenty minutes, maybe next time you're on the toilet and watch it because you will not want to miss it for the scenery alone.", author_id: author5.id)
 
 # Comment seeding
-comment1 = Comment.create(comment: "Amazing!", post_id: post.id)
-comment2 = Comment.create(comment: "Joining!", post_id: post.id)
-comment3 = Comment.create(comment: "This is awesome!", post_id: post.id)
-comment4 = Comment.create(comment: "I fear this!", post_id: post.id)
-comment5 = Comment.create(comment: "I like fun too!", post_id: post.id)
+comment1 = Comment.create(text: "Amazing!", post_id: post1.id)
+comment2 = Comment.create(text: "Joining!", post_id: post2.id)
+comment3 = Comment.create(text: "This is awesome!", post_id: post3.id)
+comment4 = Comment.create(text: "I fear this!", post_id: post4.id)
+comment5 = Comment.create(text: "I like fun too!", post_id: post5.id)
 
 puts "✅ Done seeding!"
