50f6fcd35a93cabd106460d6720816da7f7cd632
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-06 21:27:24 +0300

    create a navbar

---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Navbar(){
+
+    return(
+        <div>
+            <ul>
+                <li>About Us </li>
+                <li>Contact Us </li>
+                <li>New Us </li>
+                <li>Home Us </li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Navbar;
