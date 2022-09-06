629340931bbd75d5145c63f14c2173c7656132cf
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-07 00:05:36 +0300

    style the navbar

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -4,12 +4,36 @@ import { NavLink } from "react-router-dom";
 function Navbar(){
 
     return(
-        <div>
+        <div className="navb">
             <ul>
-                <li><NavLink to="/about">About Us </NavLink></li>
-                <li><NavLink to="/contact">Contact Us </NavLink></li>
-                <li><NavLink to="/new">New </NavLink></li>
-                <li><NavLink to="/">Home </NavLink></li>
+                <li><NavLink to="/about" style={({ isActive }) => ({
+              color: isActive ? '#fff' : '#545e6f',
+              background: isActive ? '#7600dc' : '#f0f0f0',
+              textDecoration: 'none',
+              borderRadius: '40px',
+              padding: '4px'
+            })}  >About Us </NavLink></li>
+                <li><NavLink to="/contact"  style={({ isActive }) => ({
+              color: isActive ? '#fff' : '#545e6f',
+              background: isActive ? '#7600dc' : '#f0f0f0',
+              textDecoration: 'none',
+              borderRadius: '40px',
+              padding: '4px'
+            })} >Contact Us </NavLink></li>
+                <li><NavLink to="/new"  style={({ isActive }) => ({
+              color: isActive ? '#fff' : '#545e6f',
+              background: isActive ? '#7600dc' : '#f0f0f0',
+              textDecoration: 'none',
+              borderRadius: '40px',
+              padding: '4px'
+            })} >New </NavLink></li>
+                <li><NavLink to="/"  style={({ isActive }) => ({
+              color: isActive ? '#fff' : '#545e6f',
+              background: isActive ? '#7600dc' : '#f0f0f0',
+              textDecoration: 'none',
+              borderRadius: '40px',
+              padding: '4px'
+            })} >Home </NavLink></li>
             </ul>
         </div>
     )
