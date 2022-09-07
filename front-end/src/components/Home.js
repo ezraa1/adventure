8a46e3698a17cf52360677cd8dea98bd6d20d022
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-07 12:17:49 +0300

    create card for fetched data

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-function Home(){
+function Home({title, content, author}){
+    fetch("http://localhost:9292/comments")
+    .then((r) => r.json())
+    .then((data) => console.log(data));
+
 return(
-    <div></div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+        {content}
+        </Card.Text>
+        <Button variant="primary">Read More</Button>
+      </Card.Body>
+    </Card>
 )
 }
 export default Home;
