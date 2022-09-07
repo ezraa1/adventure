fb6072687d18265c1499282befa55171ed8d74ec
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-07 10:37:23 +0300

    add routes to fetch data

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -2,13 +2,18 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  # get "/" do
-    # authors = Author.all
-    # authors.to_json
-   get "/comments" do
-    # posts = Post.all
-    # posts.to_json
+  get "/authors" do
+    authors = Author.all
+    authors.to_json
+  end
+   get "/posts" do
+    posts = Post.all
+    posts.to_json
     
+  end
+  get "/comments" do
+    comments = Comment.all
+    comments.to_json
   end
 
 end
