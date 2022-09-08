7ce25b09b801dec18b864e01908cfa5c10150e83
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-08 20:42:15 +0300

    add form to fetch data

---
+++ front-end/src/components/Post.js
@@ -0,0 +1,19 @@
+import React from "react";
+import NewForm from "./NewForm";
+
+
+function Post(){
+
+
+    return(
+        <div>
+     
+           
+            <NewForm/>
+            
+        </div>
+        
+    )
+}
+
+export default Post;
