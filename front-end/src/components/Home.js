cd16b8aa18b4bae0dd98cdfe42bfbf05ebec767d
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-09 17:18:37 +0300

    config file

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -3,7 +3,26 @@ import NewForm from "./NewForm";
 import Post from "./Post";
 import PostDelete from "./PostDelete";
 
-function Home({title, content, author}){
+const Card = (props) => {
+  return (  
+    <div className="card">
+      <a href={props.data.url}><h5 className="card-header">{props.data.title}</h5></a>
+      <img className="image" src="adv1.jpg" alt="alt message"></img>
+      <div className="card-body">{props.data.content}
+        {/* <h4 className="card-title">{props.data.title}</h4> */}
+      </div>
+    </div>
+  )
+}
+
+function cardData(posts) {
+  var values = Object.values(posts);
+  return values.map((info, idx) => {
+    return <Card data={info} key={idx} />;
+  })
+}
+
+function Home({title, content, author, onDeletePost}){
 const [posts, setPosts] =useState([])
 
 const [show, setShow] = useState(false);
@@ -11,49 +30,32 @@ useEffect(() => {
   fetch('http://localhost:9292/posts' )
     .then((r) => r.json())
     .then((posts) => setPosts(posts));
+    
 }, []);
 
 function handleClick(){
   setShow(show=>!show)
 }
-function handleNewPost(newFormData){
-  const serverOptions={
-      method:"POST",
-      headers:{
-      "Content-Type":"application/json",
-      
-      },
-      body:JSON.stringify(newFormData
-      )
-      }
-      fetch('http://localhost:9292/posts', serverOptions)
-      .then(r=>r.json())
-      .then(addedPost=>setPosts(posts=>[...posts, addedPost]))
-  }
+
  
   function handleDeleteClick(id) {
+    fetch(`http://localhost:9292/posts/${posts.id}`, {
+		  method: "DELETE",
+		})
+		.then((r) => r.json())
+		.then((post) => onDeletePost(post));
+	  
     if (window.confirm("Do you want to delete this post?")){
     posts.splice(id,1);
     }
-    console.log([...posts])
+    setPosts([...posts])
       }
 
        return(
         <div>
-        {posts.map((posts)=>(<Post key={posts.id} title={posts.title} content={posts.content} author = {posts.author} >
-    
-    </Post>)) }
-<div>
-
-      <button onClick={handleClick} >{show? "Close":"Create "} Post</button><br></br>
-      {show ? <NewForm onSubmission={handleNewPost} />: null}
-     
-      </div>
-      <div>
-      {title}
-      {content}
-</div>
-  <div>
+        
+        {cardData(posts)}
+    <div>
         <PostDelete
                                  key={posts.id}
                                  posts={posts}
@@ -62,6 +64,14 @@ function handleNewPost(newFormData){
                               />
                               
   </div>
+<div>
+
+      {/* <button onClick={handleClick} >{show? "Close":"Create "} Post</button><br></br> */}
+      {/* {show ? <NewForm onSubmission={handleNewPost} />: null} */}
+     
+      </div>
+     
+ 
       </div>
     )
     
---
+++ front-end/src/components/New.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import NewForm from './NewForm';
+
+function New(){
+    const [posts, setPosts] =useState([])
+    function handleNewPost(newFormData){
+        const serverOptions={
+            method:"POST",
+            headers:{
+            "Content-Type":"application/json",
+            
+            },
+            body:JSON.stringify(newFormData
+            )
+            }
+            fetch('http://localhost:9292/posts', serverOptions)
+            .then(r=>r.json())
+            .then(addedPost=>setPosts(posts=>[...posts, addedPost]))
+        }
+
+    return(
+ <div>
+<NewForm onSubmission={handleNewPost}/>
+ </div>
+
+    )
+}
+
+export default New;
