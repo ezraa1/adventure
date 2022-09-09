811ca0029aabf178c85424dd99ae4ae860c8f18e
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-09 11:37:26 +0300

    change fetch path

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NewForm from "./NewForm";
+import Post from "./Post";
+import PostDelete from "./PostDelete";
 
-
-function Home(){
+function Home({title, content, author}){
 const [posts, setPosts] =useState([])
 
 const [show, setShow] = useState(false);
-
+useEffect(() => {
+  fetch('http://localhost:9292/posts' )
+    .then((r) => r.json())
+    .then((posts) => setPosts(posts));
+}, []);
 
 function handleClick(){
   setShow(show=>!show)
@@ -16,7 +21,7 @@ function handleNewPost(newFormData){
       method:"POST",
       headers:{
       "Content-Type":"application/json",
-      'Accept': 'application/json'
+      
       },
       body:JSON.stringify(newFormData
       )
@@ -26,12 +31,37 @@ function handleNewPost(newFormData){
       .then(addedPost=>setPosts(posts=>[...posts, addedPost]))
   }
  
+  function handleDeleteClick(id) {
+    if (window.confirm("Do you want to delete this post?")){
+    posts.splice(id,1);
+    }
+    console.log([...posts])
+      }
 
        return(
+        <div>
+        {posts.map((posts)=>(<Post key={posts.id} title={posts.title} content={posts.content} author = {posts.author} >
+    
+    </Post>)) }
 <div>
+
       <button onClick={handleClick} >{show? "Close":"Create "} Post</button><br></br>
       {show ? <NewForm onSubmission={handleNewPost} />: null}
      
+      </div>
+      <div>
+      {title}
+      {content}
+</div>
+  <div>
+        <PostDelete
+                                 key={posts.id}
+                                 posts={posts}
+                                onDeleteClick={handleDeleteClick}
+         
+                              />
+                              
+  </div>
       </div>
     )
     
