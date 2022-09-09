83cc6ac7d32d0c4c1ea6abda5a65a4789219075b
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-09 11:44:35 +0300

    delete repeated server request

--- front-end/src/components/PostsContainer.js
+++ front-end/src/components/PostsContainer.js
@@ -1,33 +1,27 @@
 import React, {useEffect, useState} from "react";
 
 import Post from "./Post"
+import PostDelete from "./PostDelete";
 
 function PostsContainer(){
     const [posts, setPosts]= useState([])
 
    
 
-    useEffect(() => {
-        fetch('http://localhost:9292/posts' )
-          .then((r) => r.json())
-          .then((posts) => setPosts(posts));
-      }, []);
+    
 
 
-//   function handleDeleteClick(id) {
-// if (window.confirm("Do you want to delete this post?")){
-// posts.splice(id,1);
-// }
-// setPosts([...posts])
-//   }
+
     return(
         <div>
 
 {posts.map((posts)=>(<Post key={posts.id} title={posts.title} content={posts.content} author = {posts.author} >
-  
+    
   </Post>)) }
   
-        </div>
+        
+      
+  </div>
     )
 }
 
