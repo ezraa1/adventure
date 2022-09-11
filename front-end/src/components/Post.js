2134a719f16bdfe86e1abfd34d61033508ac289e
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-11 08:24:27 +0300

    fetch images

--- front-end/src/components/Post.js
+++ front-end/src/components/Post.js
@@ -2,13 +2,14 @@ import React from "react";
 
 
 
-function Post({title, content, author,onDeletePost}){
+function Post({title, content, author,image}){
 
     
     return(
         <div>
      
            <h3>{title}</h3> 
+           {image}
             <p>{content}</p>
             <h4>{author}</h4>
 
