bd1d2af15ef4ad208fc94a478bc55e9bcfb42df6
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-09-11 13:12:43 +0300

    create data on about section

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-function About(){
+function About() {
+//   const imgUrl =
+//     "https://images.unsplash.com/photo-1527786356703-4b100091cd2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80";
+  return (
+    <div className="abt">
+      {/* <img src={imgUrl} alt="travelImage" height={780} /> */}
 
-
+      <p><h2>A TRAVEL BLOG FEATURING ONE COUNTRY OR PLACE A MONTH!</h2></p>
+      <p><h5>
+        Misadventures, Drones, Photography, Itineraries, Opinions, Our Two
+        Cents, Videos, The Best & Worst Countries To Travel To If You Have Food
+        Allergies, Part Time Travel and More!!!! Oh Yeah, and this is definitely
+        not the blog for you if you want to learn how to travel light!{" "}
+        </h5>
+      </p>
+    </div>
+  );
 }
 export default About;
